ef87e498548fb5f64a03c8d7a45789c6536f7d66
Santiago Benenatti <sbenenatti@gmail.com>
2024-05-21 10:42:13 -0300

    termine el carrusel

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const imagenes=['josegarcesss.github.io\imagenes\brazil.png','josegarcesss.github.io\imagenes\argentina.png','josegarcesss.github.io\imagenes\chile.png'];
+const imagenes=['imagenes/argentina.png','imagenes/playa.jpg','imagenes/chile.png'];
 let n=0;
-const carrucel=document.getElementById('carousel-image');
-document.getElementById('back').addEventListener('click',()=>{n==(n=0)?imagenes.length-1:n-1;
-    caroucel.src=imagenes[n];});
-document.getElementById('next').addEventListener('click',()=>{n==(n=0)?imagenes.length-1:n+1;
-    caroucel.src=imagenes[n];});  
+const caroucel=document.getElementById('carousel-image');
+
+
+document.getElementById('back').addEventListener('click',()=>{n = (n===0) ? imagenes.length - 1 : n-1;
+    caroucel.src=imagenes[n];
+});
+
+
+document.getElementById('next').addEventListener('click',()=>{n = (n=== imagenes.length-1) ? 0 : n+1;
+    caroucel.src=imagenes[n];
+});  
